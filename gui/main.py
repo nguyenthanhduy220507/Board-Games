d55ffa403f3d9f837ac95625ad0cf0d519b85731
QuocDuong16 <nguyenquocduong380@gmail.com>
2023-04-13 19:56:15 +0700

    Update mainmenu

--- gui/main.py
+++ gui/main.py
@@ -1,22 +1,41 @@
 import pygame
 import pygame_menu
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((1080, 720))
 
+class MainMenu:
+    def __init__(self, screen, width, height):
+        self.screen = screen
+        self.width = width
+        self.height = height
+
+        # Tạo đối tượng Menu
         self.menu = pygame_menu.Menu(
-            title='Welcome to board game',
-            width=1080,
-            height=720,
-            theme=pygame_menu.themes.THEME_DEFAULT
-        )
+            'Main Menu', width, height, theme=pygame_menu.themes.THEME_BLUE)
 
-    def run(self):
+        # Thêm các tùy chọn vào menu
+        self.menu.add.button('Start the game', self.start_the_game)
+        self.menu.add.button('Quit', pygame_menu.events.EXIT)
+
+    def start_the_game(self):
+        # Chuyển đến trò chơi
+        game_loop()
+
+    def show(self):
+        # Hiển thị menu
         self.menu.mainloop(self.screen)
 
+# Vòng lặp chính của trò chơi
+def game_loop():
+    # Làm gì đó ở đây
+    pass
+    # Khởi tạo pygame
 
-if __name__ == '__main__':
-    menu = Game()
-    menu.run()
+pygame.init()
+# Khởi tạo màn hình
+screen = pygame.display.set_mode((640, 480))
+
+# Tạo đối tượng MainMenu
+main_menu = MainMenu(screen, 640, 480)
+
+# Hiển thị menu
+main_menu.show()
