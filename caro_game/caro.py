e4206ff5e058a8bfaf719e55e16f65757a7130b8
Nguyễn Thanh Duy <nguyenthanhduy220507@gmail.com>
2023-04-17 14:52:53 +0700

    update

--- caro_game/caro.py
+++ caro_game/caro.py
@@ -1,1 +1,41 @@
-print("")
+import pygame
+import numpy as np
+import sys
+from GameSettings.DefaultSettings import *
+from GameSettings import LightTheme
+from GameSettings import Caro20x20
+
+class BoardGUI():
+    def __init__(self, screen, theme, row, col):
+        super().__init__(row, col)
+        self.screen = screen
+        self.theme = theme
+        self.size = Caro20x20
+        self.number_score_to_win = self.size.number_square_to_win
+
+    def drawBoardGames(self):
+        self.screen.fill(self.theme.background_color)
+        # Draw vertical lines
+        x_index = self.square_size
+        for col in range(self.board_col):
+            pygame.draw.line(self.screen, self.theme.line_color, (x_index, 0), (x_index, board_height), self.size.line_width)
+            x_index += self.square_size
+        # Draw horizontal lines
+        y_index = self.square_size
+        for row in range(self.board_col):
+            pygame.draw.line(self.screen, self.theme.line_color, (0, y_index), (board_width, y_index), self.size.line_width)
+            y_index += self.square_size
+
+width, height = 640, 640
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Trò chơi cờ caro')
+
+def run():
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+
+run()
---
+++ caro_game/hcn.py
@@ -0,0 +1,23 @@
+class Hcn:
+    x1= 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
+    maunen = "white"
+    duongvien = "black"
+    def __init__(self, x1 = 0, y1 = 0, x2 = 0, y2= 0, maunen = "white", duongvien = "black"):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.duongvien = duongvien
+        self.maunen = maunen
+    def vehinh (self, cas):
+        self.hinh = cas.create_rectangle(self.x1,self.y1,self.x2,self.y2,outline = self.duongvien,fill = self.maunen)
+
+    #kiểm tra ô trong bàn cờ trống
+    def check_input_mouse(self, x, y):
+        if x > self.x1 and x < self.x2 and y > self.y1 and y <self.y2:
+            return 1
+        else:
+            return 0
