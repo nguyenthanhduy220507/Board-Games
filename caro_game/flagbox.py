d9e9cb9ba4ce3e9ae4173c1cc801395f758ee6ad
Nguyễn Thanh Duy <nguyenthanhduy220507@gmail.com>
2023-04-17 17:10:23 +0700

    update

--- caro_game/flagbox.py
+++ caro_game/flagbox.py
@@ -7,13 +7,13 @@ class FlagBox(Square):
         super().__init__(mx1,my1,mx2,my2)
         self.giatri = 0
     
-    def vehinh(self, screen):
+    # vẽ sự kiên X O
+    def vehinh(self,screen):
         super().vehinh(screen)
-        dx = self.square_size / 4 # độ dài của cạnh /4
-        dy = self.square_size / 4
-        if self.giatri == 1: # hình tròn
-            pygame.draw.ellipse(screen, (255, 255, 255), (self.x + dx, self.y + dy, self.square_size - 2 * dx, self.square_size - 2 * dy))
-        if self.giatri == -1: # hình vuông
-            pygame.draw.line(screen, (255, 255, 255), (self.x + dx, self.y + dy), (self.x + self.square_size - dx, self.y + self.square_size - dy), 10)
-            pygame.draw.line(screen, (255, 255, 255), (self.x + self.square_size - dx, self.y + dy), (self.x + dx, self.y + self.square_size - dy), 10)
-
+        dx = (self.x2 - self.x1)/4 # độ dài của cạnh /4
+        dy = (self.y2 - self.y1)/4
+        if self.giatri == 1: #hình tròn
+            pygame.draw.ellipse(screen, (255, 255, 255), (self.x1 ,self.y1 , self.x, self.y2))
+        if self.giatri == -1: # hình square
+            pygame.draw.line(screen, (255, 255, 255), (self.x1, self.y1 ), (self.x2 , self.y2 ), 10)
+            pygame.draw.line(screen, (255, 255, 255), (self.x2, self.y1 ), (self.x1 , self.y2 ), 10)
