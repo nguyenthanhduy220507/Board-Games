67d3fab98b7b6fe9890d4deabb26942753af7a3c
Nguyễn Thanh Duy <nguyenthanhduy220507@gmail.com>
2023-04-19 13:29:35 +0700

    update

--- caro_game/flagbox.py
+++ caro_game/flagbox.py
@@ -10,14 +10,28 @@ class FlagBox(Square):
     # vẽ sự kiên X O
     def vehinh(self,screen):
         super().vehinh(screen)
-        # dx = (self.x2 - self.x1)/4 # độ dài của cạnh /4
-        # dy = (self.y2 - self.y1)/4
         if self.giatri == 1: #hình tròn
-                width = abs(self.x2 - self.x1)
-                height = abs(self.y2 - self.y1)
-                radius = min(width, height) // 2
-                center = (self.x1 + width // 2, self.y1 + height // 2)
-                pygame.draw.circle(screen, (119, 192, 74), center, radius)
+        # vẽ hình chữ nhật với kích thước =hình tròn
+            rect_width = abs(self.x2 - self.x1) 
+            rect_height = abs(self.y2 - self.y1) 
+            rect_x = self.x1
+            rect_y = self.y1
+            pygame.draw.rect(screen, (0, 0, 0), (rect_x, rect_y, rect_width, rect_height))
+            
+            width = abs(self.x2 - self.x1)
+            height = abs(self.y2 - self.y1)
+            radius = min(width, height) // 2
+            center = (self.x1 + width // 2, self.y1 + height // 2)
+            pygame.draw.circle(screen, (119, 192, 74), center, radius)
+
         if self.giatri == -1: # hình square
+            # vẽ hình chữ nhật với kích thước = hình vuông
+            rect_width = abs(self.x2 - self.x1)
+            rect_height = abs(self.y2 - self.y1) 
+            rect_x = self.x1
+            rect_y = self.y1 
+            pygame.draw.rect(screen, (0, 0, 0), (rect_x, rect_y, rect_width, rect_height))
+            
             pygame.draw.line(screen, (255, 0, 0), (self.x1, self.y1 ), (self.x2 , self.y2 ), 3)
             pygame.draw.line(screen, (255, 0, 0), (self.x2, self.y1 ), (self.x1 , self.y2 ), 3)
+
