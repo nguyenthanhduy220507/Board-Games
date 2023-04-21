49b73cd3db040fb5450fe05e08728f12737d9e9a
QuocDuong16 <nguyenquocduong380@gmail.com>
2023-04-21 17:12:54 +0700

    update

--- play_surface.py
+++ play_surface.py
@@ -156,7 +156,6 @@ class PlaySurface:
 
     def run(self):
         while True:
-            dt = self.game_gui_window.clock.tick() / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                     self.connection.close()
@@ -165,6 +164,7 @@ class PlaySurface:
                 self.game_gui_window.editor.event_loop(event)
                 self.game_gui_window.mouse_event(event)
 
+            dt = self.game_gui_window.clock.tick(60)
             self.game_gui_window.editor.run()
             pygame.display.update()
             self.chat_gui_window.window.update()
