e3a796cad3e80384b0294ea64704a0207e2c309d
QuocDuong16 <nguyenquocduong380@gmail.com>
2023-04-22 14:17:07 +0700

    update

--- caro_game/setting.py
+++ caro_game/setting.py
@@ -6,6 +6,6 @@ WINDOW_HEIGHT = 640
 LINE_COLOR = "black"
 
 EDITOR_DATA = [
-    {'id': 'x', 'image': './img/mark_x.png'},
-    {'id': 'o', 'image': './img/mark_o.png'}
+    {'id': 'x', 'image': './img/mark_x_1.png'},
+    {'id': 'o', 'image': './img/mark_o_1.png'}
 ]
